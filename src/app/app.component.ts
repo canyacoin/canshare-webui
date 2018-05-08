20ba700de144d28092d829e83ee0178a2bbf08e7
Gus <gustavo@canya.com>
2018-05-08 11:32:07 -0500

    Adds bancor widget

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
 
+declare var BancorConvertWidget: any;
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'app';
+
+  ngAfterViewInit() {
+    BancorConvertWidget.init({
+      'type': '1',
+      'baseCurrencyId': '5a6f61ece3de16000123763a',
+      'pairCurrencyId': '5937d635231e97001f744267',
+      'primaryColor': '#00BFFF',
+      'primaryColorHover': '55DAFB'
+    });
+  }
 }
